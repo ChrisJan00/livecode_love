19104ead78b79a6cd43a3c888c68208040cb5e7a
ChrisJan00 <cjan_ynzn@yahoo.es>
2014-07-19 16:03:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,88 @@
+livecode_love
+=============
+by Christiaan Janssen
+based on LICK
+
+Simple livecoding environment for Löve2D.
+Overwrites love.run, pressing all errors to the terminal/console
+
+
+Usage:
+------
+
+require "livecode" 
+
+at the beginning on main.lua.  No further changes needed
+
+
+When you load extra files via love.filesystem.load(..), they will be tracked by the system.
+
+Each time you save one of these files it will be reloaded automatically and the changes applied.
+
+Additionally, the new callback love.livereload will be called if it exists. 
+
+
+Control flags:
+--------------
+ - livecode.resetOnLoad -> if set to "true", love.load() will be called when reloading files (instead of love.livereload)
+ - livecode.logReloads  -> if set to "true", the message "updated file _FILENAME_" will be printed on the console output each time _FILENAME_ is reloaded
+ - livecode.reloadOnF5  -> if set to "true", calls love.load() when you press F5 from within the game, effectively resetting the game livecode.showErrorOnScreen -> When an error occurs, the error message is printed on the console by default.  You can correct the error and the file will be automatically reloaded and execution resumed on save (no need to restart the game). However, the game screen will apear plain black.  This is done on purpose, so that your state (transformations, color, current font) is untouched and will be kept when resuming.  If you want to see the error message in the game screen, set this flag to "true".  Font and transformations will be restored when resuming, but other changes (e.g. current canvas, scissors) will be lost.  If you draw function restitutes them, this will not be a problem.
+
+For changing the flags, the easiest way is to include this library by
+livecode = require "livecode"
+
+Then, for example
+livecode.reloadOnF5 = false
+
+
+Known limitations
+-----------------
+
+If you have errors on initialization of the game, the library might not be able to capture them.  You can recognize the situation because the error screen will be the default blue from Löve2D.  In that case, you need to close the game and relaunch it when you fix the errors.
+
+Depending on your coding style, the changes in your code might not be applied.  For example:
+
+
+	function love.load()
+		EntityList = {}
+		table.insert(EntityList, { x = 0 })
+	end
+
+	function updateEntity(entity, dt)
+		print(entity.x)
+	end
+
+	function love.update(dt)
+		for i,entity in ipairs(EntityList) do
+			updateEntity(entity, dt)
+		end
+	end
+
+In this example, changes in the body of updateEntity will be applied on reload,
+since this is a global function.  Reloading the file will overwrite this function.  The global function is called within love.update.
+
+But if the example had different code in love.load and update:
+
+	function love.load()
+		EntityList = {}
+		table.insert(EntityList, { x = 0, update = updateEntity })
+	end
+
+	function love.update(dt)
+		for i,entity in ipairs(EntityList) do
+			entity:update(dt)
+		end
+	end
+
+In this case, reloading the file after making changes in updateEntity still overwrites it, but the entity itself is still using the old code.  Reloading the file will not show the changes in your update code.  The livereload callback is provided for covering this case, in this example a possibility would be:
+
+	function love.livereload()
+		for i,entity in ipairs(EntityList) do
+			entity.update = updateEntity
+		end
+	end
+
+This ensures that the entity itself calls the latest version of updateEntity. Livereload will be called after the file is reloaded, in that point in time the function has already been overwritten.
+
+
+-- Christiaan Janssen, July 2014
---
+++ livecode.lua
@@ -0,0 +1,189 @@
+-- livecode.lua 
+-- by Christiaan Janssen
+-- based on LICK
+--
+-- Simple livecoding environment for Löve2D.
+-- Overwrites love.run, pressing all errors to the terminal/console
+--
+-- Usage:
+-- require "livecode" 
+-- at the beginning on main.lua.  No further changes needed
+--
+-- When you load extra files via love.filesystem.load(..), they will be tracked by the system.
+-- Each time you save one of these files it will be reloaded automatically and the changes applied.
+-- Additionally, the new callback love.livereload will be called if it exists. 
+--
+-- Control:
+-- livecode.resetOnLoad -> if set to "true", love.load() will be called when reloading files (instead of love.livereload)
+-- livecode.logReloads  -> if set to "true", the message "updated file _FILENAME_" will be printed on the console output 
+--                        each time _FILENAME_ is reloaded
+-- livecode.reloadOnF5  -> if set to "true", calls love.load() when you press F5 from within the game, effectively resetting the game
+-- livecode.showErrorOnScreen -> When an error occurs, the error message is printed on the console by default.  You can correct the
+--                          error and the file will be automatically reloaded and execution resumed on save (no need to restart the game)
+--                          however, the game screen will apear plain black.  This is done on purpose, so that your state (transformations,
+--                          color, current font) is untouched and will be kept when resuming.  If you want to see the error message in
+--                          the game screen, set this flag to true.  Font and transformations will be restored when resuming, but other
+--                          changes (e.g. current canvas, scissors) will be lost.  If you draw function restitutes them, this will not
+--                          be a problem.
+
+local livecode = {}
+livecode.resetOnLoad = false
+livecode.logReloads = true
+livecode.reloadOnF5 = true
+livecode.showErrorOnScreen = true
+
+local errorHappened = false
+local errorMsg = ""
+local timestamps = { ["main.lua"] = love.filesystem.getLastModified("main.lua") }
+local storedFont = nil
+
+-- override filesystem.load
+local orig_loadfile = love.filesystem.load
+local function new_loadfile(filename)
+    timestamps[filename] = love.filesystem.exists(filename) and love.filesystem.getLastModified(filename)
+    return orig_loadfile(filename)
+end
+love.filesystem.load = new_loadfile
+
+
+-- error handling
+local function storeState()
+    love.graphics.push()
+    love.graphics.reset()
+    love.graphics.origin()
+    storedFont = love.graphics.getFont()
+    love.graphics.setNewFont(math.floor(14 * love.window.getPixelScale()))
+end
+
+local function recoverState()
+    love.graphics.pop()
+    love.graphics.setFont(storedFont)
+end
+
+local function drawError()
+    local pos = 20 * love.window.getPixelScale()
+    love.graphics.clear()
+    love.graphics.print(errorMsg, pos, pos)
+    love.graphics.present()
+end
+
+local function manageError(msg)
+    errorHappened = true
+
+    local p = (debug.traceback("Error: " .. tostring(msg), 2):gsub("\n[^\n]+$", ""))
+    p = string.gsub(p, "%[string \"(.-)\"%]", "")
+    p = string.gsub(p, "\n\t+%[C%]: in function 'xpcall'", "")
+
+    if livecode.showErrorOnScreen then
+        storeState()
+        errorMsg = p
+    end
+    print(p)
+end
+
+local function disableError()
+    if errorHappened and livecode.showErrorOnScreen then
+        recoverState()
+    end
+    errorHappened = false
+end
+
+-- update function
+local function update(dt)
+    local ok
+    local anyFileModified = false
+    for filename,timestamp in pairs(timestamps) do
+        if timestamp < love.filesystem.getLastModified(filename) then
+            if livecode.logReloads then
+                print("updated file "..filename)
+            end
+            disableError()
+            ok, chunk = xpcall(function() return love.filesystem.load(filename) end, manageError)
+            if not ok then break end
+
+            if xpcall(chunk, manageError) then
+                anyFileModified = true
+            end
+        end
+    end
+
+    if anyFileModified then
+        if livecode.resetOnLoad then
+            xpcall(love.load, manageError)
+        elseif love.livereload then
+            xpcall(love.livereload, manageError)
+        end
+    end
+
+    if not errorHappened then
+        pcall(function() love.update(dt) end, manageError)
+    end
+end
+
+
+local function draw()
+    if not errorHappened then
+        xpcall(love.draw, manageError)
+    elseif livecode.showErrorOnScreen then
+        drawError()
+    end
+end
+
+
+function love.run()
+    math.randomseed(os.time())
+    math.random() math.random()
+
+    if love.event then
+        love.event.pump()
+    end
+
+    if love.load then love.load(arg) end
+
+
+    local dt = 0
+
+    -- Main loop time.
+    while true do
+        -- Process events.
+        if love.event then
+            love.event.pump()
+            for e,a,b,c,d in love.event.poll() do
+            if e == "quit" then
+                if not love.quit or not love.quit() then
+                    if love.audio then
+                        love.audio.stop()
+                    end
+                return
+                end
+            end
+
+            ok = xpcall(function() love.handlers[e](a,b,c,d) end, manageError)
+            if livecode.reloadOnF5 and a=="f5" and e == "keypressed" then
+                disableError()
+                xpcall(love.load, manageError)
+            end
+        end
+    end
+
+    -- Update dt, as we'll be passing it to update
+    if love.timer then
+        love.timer.step()
+        dt = love.timer.getDelta()
+    end
+
+    -- Call update and draw
+    if love.update then update(dt) end -- will pass 0 if love.timer is disabled
+
+    if love.window and love.graphics then
+        love.graphics.clear()
+        love.graphics.origin()
+        if love.draw then draw() end
+        love.graphics.present()
+    end
+
+    if love.timer then love.timer.sleep(0.001) end
+  end
+end
+
+return livecode
