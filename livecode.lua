96d7bf480eaaae0d98c23cb35dd88fca940859fb
ChrisJan00 <cjan_ynzn@yahoo.es>
2015-01-14 16:43:40 +0100

    added license text

--- livecode.lua
+++ livecode.lua
@@ -31,6 +31,27 @@
 --                      beginning of saving it. If we reload an image at that point, the file will be "corrupted".
 --                      to prevent that, "delay" is meant to give time to gimp to save the thing.
 
+
+-- zlib license
+-- Copyright (c) 2014-2015 Christiaan Janssen
+
+-- This software is provided 'as-is', without any express or implied
+-- warranty. In no event will the authors be held liable for any damages
+-- arising from the use of this software.
+
+-- Permission is granted to anyone to use this software for any purpose,
+-- including commercial applications, and to alter it and redistribute it
+-- freely, subject to the following restrictions:
+
+-- 1. The origin of this software must not be misrepresented; you must not
+--    claim that you wrote the original software. If you use this software
+--    in a product, an acknowledgement in the product documentation would be
+--    appreciated but is not required.
+-- 2. Altered source versions must be plainly marked as such, and must not be
+--    misrepresented as being the original software.
+-- 3. This notice may not be removed or altered from any source distribution.
+
+
 local livecode = {}
 livecode.resetOnLoad = false
 livecode.logReloads = true
